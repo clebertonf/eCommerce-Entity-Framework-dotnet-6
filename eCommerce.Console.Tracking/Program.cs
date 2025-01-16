052d5e25ea26990ae42255d36fee85789544e21a
Cleberton <clebertonfgc@gmail.com>
2025-01-16 20:16:01 -0300

    final changeTracker debug.

--- eCommerce.Console.Tracking/Program.cs
+++ eCommerce.Console.Tracking/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using eCommerce.Console.Tracking;
+using eCommerce.Models.Models;
 using Microsoft.EntityFrameworkCore;
 
 var db = new ApplicationDbContext();
@@ -14,6 +15,13 @@ var user2 = db.Users.AsNoTracking().FirstOrDefault(u => u.Id == 1);
 user2.Name = "John Doe";
 db.Update(user2);
 db.SaveChanges();
+
+// Adds object to Efcore track
+var user3 = new User() { Id = 1, Name = "John" };
+db.Users.Attach(user3);
+db.Entry(user3).Property(x => x.Name).IsModified = true;
+
+// Console.WriteLine(db.ChangeTracker.DebugView.LongView); Debugging efcore tracing
 
 // It clears up the content and is no longer accompanied by the EFcore
 db.ChangeTracker.Clear();
